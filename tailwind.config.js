08c3e52bf55498e8da4a457eec9b63f27cb3e608
adilsson diaz <adilssondiaz557@gmail.com>
2024-12-16 14:39:42 -0500

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Agrega esto para que Tailwind analice tus archivos HTML y TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
